dea04aa217b41691eda9237e6185f6e402fcd9ec
Gladys Chen Cheng <chen0552@algonquinlive.com>
2024-07-18 13:24:58 -0400

    create the PriceList component
    1. introduce bootstrap css lib
    2. create PriceList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import PriceList from './components/PriceList';
+const items = [
+  {
+    "id": 1,
+    "title": "Travel",
+    "price": 200,
+    "date": "2018-09-10",
+    "category": {
+      "id": "1",
+      "name": "Travel",
+      "type": "outcome"
+    }
+  },
+  {
+    "id": 2,
+    "title": "Travel",
+    "price": 400,
+    "date": "2018-09-10",
+    "category": {
+      "id": "1",
+      "name": "Travel",
+      "type": "outcome"
+    }
+  }
+]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Welcome to React</h1>
+        </header>
+        <PriceList 
+        items={items} 
+        onModifyItem={(item)=>{alert(item.id)}}
+        onDeleteItem={(item)=>{alert(item.id)}}
+        />
+      </div>
+    )
+  }
 }
+
 
 export default App;
